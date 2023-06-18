5e217604eb4592b902a978568c03ebf8bdd48a54
antoniogisondi <anto.gisondi@tiscali.it>
2023-06-19 01:09:14 +0200

    [DOCS] Exercise done

--- js/main.js
+++ js/main.js
@@ -3,7 +3,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            
+            // VARIABILI CHE SERVONO PER LE FUNZIONI
+            email: '',
+            array_email:[]
         }
     },
+
+    methods: {
+        // FUNZIONE CHE TI CREA 10 EMAIL 
+            createTenEmail(){
+            for(let i=0; i < 10; i++){
+                // CHIAMATA DELL ENDPOINT CON FUNZIONE CHE ESEGUE LA CHIAMATA ALL'ENDPOINT
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                this.array_email.push(result.data.response)
+            })
+            }
+        }
+    }
 }).mount('#app')
